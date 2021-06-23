82f659c0c3f51d18c9c0332a53d32a0fa6c149d9
FairRing <kjh122333@gmail.com>
2021-06-24 00:32:58 +0900

    done...

--- Q15_Iceberg/src/Main.java
+++ Q15_Iceberg/src/Main.java
@@ -1,45 +1,101 @@
 import java.io.*;
 import java.util.*;
+import java.awt.Point;
 
 public class Main {
-    static int n, m; // 세로, 가로
-    static int[][] iceberg;
-    static int[][] seen; // true = 1  OR  false =0
-    static int[] aroundX = {1,-1,0,0};
-    static int[] aroundY = {0,0,-1,1};
-    static int year = 0;// 빙산이 분리되는 최초 년도
+    static int n, m, answer = 0; // 세로, 가로, 정답
+    static int arr[][], map[][];
+    static int aroundX[] = {1,-1,0,0}, aroundY[] = {0,0,-1,1};
+    static ArrayList<Point> icebergs = new ArrayList<Point>(); // 빙산
 
-    static class Point {
-        int n, m, h;
-        public Point (int n, int m, int h) {
-            this.n = n; // 세로
-            this.m = m; // 가로
-            this.h = h; // 높이
+    //연결 체크
+    public static int getNumberOfIcebergs(Point icebergLocation) {
+        int count = 0;
+        boolean visit[][] = new boolean[n][m];
+        Queue<Point> queue = new LinkedList<Point>();
+        queue.add(icebergLocation);
+        visit[icebergLocation.y][icebergLocation.x] = true;
+
+        while(!queue.isEmpty()) { 
+            Point point = queue.poll();
+            count++;
+            for(int i=0; i<4; i++) {
+                int ay = point.y + aroundY[i];
+                int ax = point.x + aroundX[i];
+                if(0<=ay && ay<n && 0<=ax && ax<m ) {  // check border lines...
+                    if(arr[ay][ax]!=0 && !visit[ay][ax]) { // find new iceberg location...
+                        visit[ay][ax]=true;
+                        queue.add(new Point(ax,ay));
+                    }
+                }
+            }
         }
+        return count;
     }
-
+ 
     public static void bfs() {
-        
+        Queue<Point> queue = new LinkedList<Point>();
+
+        for (int i = 0; i < icebergs.size(); i++) {
+            Point point = icebergs.get(i);
+            queue.add(point);
+        }
+
+        while (!queue.isEmpty()) {
+            int queueSize = queue.size();
+            answer++;
+
+            for (int i = 0; i < queueSize; i++) {
+                Point point = queue.poll();
+                int count = 0; // number of contact with sea...
+                for (int j = 0; j < 4; j++) {
+                    int ax = point.x + aroundX[j];
+                    int ay = point.y + aroundY[j];
+                    if (0 <= ax && ax < m && 0 <= ay && ay < n && arr[ay][ax] == 0)count++; // check about out of range & is location of sea...
+                }
+                if (map[point.y][point.x] <= count) { // melted away
+                    map[point.y][point.x] = 0; // minimum of iceberg's value is 0!
+                } else {
+                    map[point.y][point.x] = map[point.y][point.x] - count;
+                    queue.add(point);
+                }
+            }
+            // copy current status...
+            for (int j = 0; j < n; j++) {
+                for (int j2 = 0; j2 < m; j2++) {
+                    arr[j][j2] = map[j][j2];
+                }
+            }
+
+            if (queue.peek() != null && getNumberOfIcebergs(queue.peek()) != queue.size()) { // check icebergs exist
+                System.out.println(answer); // ANSWER
+                return;
+            }
+        }
+        System.out.println(0); // ANSWER
     }
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = reader.readLine().split(" ");
+        String input[] = reader.readLine().split(" ");
         n = Integer.parseInt(input[0]);
         m = Integer.parseInt(input[1]);
-        iceberg = new int[n][m];
-        seen = new int[n][m];
-
-        for (int n_ = 0; n_ < n; n_++) {
+        arr = new int[n][m];
+        map = new int[n][m];
+        for (int i = 0; i < n; i++) { // init loop start~
             input = reader.readLine().split(" ");
-            for (int m_ = 0; m_ < m; m_++) {
-                iceberg[n][m] = Integer.parseInt(input[0]);
+            for (int j = 0; j < m; j++) {
+                int element = Integer.parseInt(input[j]);
+                arr[i][j] = element;
+                map[i][j] = element;
+                if (element != 0) icebergs.add(new Point(j,i));
             }
-        }
-      
-        bfs(); // <<< next... retry one more...
+        } // init loop end.
 
-		// System.out.println(year);
-
-
+        // bfs start~
+        if (getNumberOfIcebergs(icebergs.get(0)) != icebergs.size()) { // check icebergs exist
+            System.out.println(0); // ANSWER
+        } else {
+            bfs();
+        } // bfs end.
     }
 }
