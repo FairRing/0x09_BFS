708a60c4b3aa63b3eed7ee34f3be0ee89edbe7fa
FairRing <kjh122333@gmail.com>
2021-06-27 13:01:11 +0900

    done...

--- Q18_StartLink/README.md
+++ Q18_StartLink/README.md
@@ -1,14 +1,44 @@
-## Getting Started
+# 스타트링크
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+- 출처 : [https://www.acmicpc.net/problem/5014](https://www.acmicpc.net/problem/5014)
 
-## Folder Structure
+## 문제
 
-The workspace contains two folders by default, where:
+> </br>강호는 코딩 교육을 하는 스타트업 스타트링크에 지원했다. 오늘은 강호의 면접날이다. 하지만, 늦잠을 잔 강호는 스타트링크가 있는 건물에 늦게 도착하고 말았다. </br></br>
+> 스타트링크는 총 F층으로 이루어진 고층 건물에 사무실이 있고, 스타트링크가 있는 곳의 위치는 G층이다. 강호가 지금 있는 곳은 S층이고, 이제 엘리베이터를 타고 G층으로 이동하려고 한다. </br></br>
+> 보통 엘리베이터에는 어떤 층으로 이동할 수 있는 버튼이 있지만, 강호가 탄 엘리베이터는 버튼이 2개밖에 없다. U버튼은 위로 U층을 가는 버튼, D버튼은 아래로 D층을 가는 버튼이다. (만약, U층 위, 또는 D층 아래에 해당하는 층이 없을 때는, 엘리베이터는 움직이지 않는다) </br></br>
+> 강호가 G층에 도착하려면, 버튼을 적어도 몇 번 눌러야 하는지 구하는 프로그램을 작성하시오. 만약, 엘리베이터를 이용해서 G층에 갈 수 없다면, "use the stairs"를 출력한다.</br></br>
 
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
+## 입력
 
-## Dependency Management
+- 첫째 줄에 F, S, G, U, D가 주어진다. (1 ≤ S, G ≤ F ≤ 1000000, 0 ≤ U, D ≤ 1000000) 건물은 1층부터 시작하고, 가장 높은 층은 F층이다.
 
-The `JAVA DEPENDENCIES` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-pack/blob/master/release-notes/v0.9.0.md#work-with-jar-files-directly).
+## 출력
+
+- 첫째 줄에 강호가 S층에서 G층으로 가기 위해 눌러야 하는 버튼의 수의 최솟값을 출력한다. 만약, 엘리베이터로 이동할 수 없을 때는 "use the stairs"를 출력한다.
+
+## 예제
+
+- 입력 1
+
+```cmd
+10 1 10 2 1
+```
+
+- 출력 1
+
+```cnd
+6
+```
+
+- 입력 2
+
+```cmd
+100 2 1 1 0
+```
+
+- 출력 2
+
+```cmd
+use the stairs
+```
--- Q18_StartLink/src/Main.java
+++ Q18_StartLink/src/Main.java
@@ -1,5 +1,48 @@
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    static int F, S, G, U, D;
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputValue = reader.readLine().split(" ");
+
+        F = Integer.parseInt(inputValue[0]); // 층 : 빌딩 꼭대기 층
+        S = Integer.parseInt(inputValue[1]); // 층 : 강호 ( START )
+        G = Integer.parseInt(inputValue[2]); // 층 : 스타트링크( GOAL )
+        U = Integer.parseInt(inputValue[3]); // 버튼 : 위로
+        D = Integer.parseInt(inputValue[4]); // 버튼 : 아래로
+        int[] B = new int[F + 1];
+        System.out.println(BFS(F, S, G, U, D, B));
+
+    }
+
+    public static String BFS(int floor, int start, int goal, int up, int down, int[] B) {
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.offer(start);
+        B[start] = 1;
+
+        while (!queue.isEmpty()) {
+            int element = queue.poll();
+            if (element == goal) {
+                return String.valueOf(B[element] - 1);
+            }
+            if (element + up <= floor) {
+                if (B[element + up] == 0) {
+                    B[element + up] = B[element] + 1;
+                    queue.offer(element + up);
+                }
+
+            }
+            if (element - down > 0) {
+                if (B[element - down] == 0) {
+                    B[element - down] = B[element] + 1;
+                    queue.offer(element - down);
+                }
+            }
+
+        }
+        return "use the stairs";
     }
 }
