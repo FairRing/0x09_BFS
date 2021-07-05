19865a863a6d62292cf7f16e1a18ab5cd9fb76cf
FairRing <kjh122333@gmail.com>
2021-07-06 00:36:58 +0900

    done.

--- Q21_HideAndSeek4/src/Main.java
+++ Q21_HideAndSeek4/src/Main.java
@@ -2,62 +2,57 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    
-    static int N, K, MIN = 0, MAX = 100000, second = 0;
-    static int[] line = new int[100001];
+    private static int N, K, SIZE = 100001;
+    private static int[] visited = new int[SIZE], parent = new int[SIZE];
 
     public static void main(String[] args) throws IOException {
+        // INPUT :: start, goal location
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String input[] = reader.readLine().split(" ");
         N = Integer.parseInt(input[0]);
         K = Integer.parseInt(input[1]);
 
-        int X = N;        
-        while (true) {
-            int distance = difference(X);
-            int cases[] = new int[3];
-
-            if (distance == 0) {
-                break;
-            } else {
-
-                cases[0] = moveForward(X);
-                cases[1] = moveBackward(X);
-                cases[2] = teleport(X);
-                Arrays.sort(cases);
-                
-                value1 = moveForward(X);
-                value2 = moveBackward(X);
-                value3 = teleport(X);
-    
-            }
-
-            
-            
-            
-
-
+        bfs(N, K); // BFS
+        
+        // OUTPUT :: second
+        System.out.println(visited[K]-1);
+        // OUTPUT :: shortest path
+        Stack<Integer> stack = new Stack<Integer>();
+        int history = K;
+        while (history != N) {
+            stack.push(history);
+            history = parent[history];
         }
+        stack.push(history);
+        while (!stack.isEmpty()) System.out.print(stack.pop()+" ");
 
+        reader.close();
     }
 
+    private static void bfs(int start, int goal) {
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.add(start);
+        visited[start] = 1;
 
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
 
-    private static int teleport(int X) {
-        if(X*2 > MAX) return -999999;
-        return X*2;
-    }
-    private static int moveForward(int X) {
-        if(X+1 > MAX) return -999999;
-        return X+1;
-    }
-    private static int moveBackward(int X) {
-        if(X-1 < MIN) return -999999;
-        return X-1;
-    }
-    private static int difference(int X) {
-        return K-X;
+            if (now+1 <= 100000 && visited[now+1] == 0) {
+                visited[now+1] = visited[now]+1;
+                parent[now+1] = now;
+                queue.add(now+1);
+            }
+            if (now-1 >= 0 && visited[now-1] == 0) {
+                visited[now-1] = visited[now]+1;
+                parent[now-1] = now;
+                queue.add(now-1);
+            }
+            if (now*2 <= 100000 && visited[now*2] == 0) {
+                visited[now*2] = visited[now]+1;
+                parent[now*2] = now;
+                queue.add(now*2);
+            }
+            if (visited[goal] != 0) return;
+        }
     }
 }
-
-
